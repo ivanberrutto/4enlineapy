36dca177e5dfc232d99f5a92f96e54fbe8a48ce3
Iván Berrutto <ivanberrutto@gmail.com>
2020-01-08 04:44:25 -0300

    Creo la accion de introducir jugadas en el array del tablero, funcion para dar vuelta el array para que sea visible y verificacion de si es valida la posicion

--- 4enlinea.py
+++ 4enlinea.py
@@ -1,26 +1,49 @@
 import numpy
 
+cantidadfilas=6
+cantidadcol=7
+#Verifica si se puede poner ficha en esa columna, entrega True si se puede(0)
+def posicionvalida(tablero, col):
+	return tablero[5][col] == 0
+
+#Hace un for que busca la primera fila de la columna elegida que tenga un 0 ( no tenga ficha )
+def proximafila(tablero, col):
+	for r in range(cantidadfilas):
+		if tablero[r][col] == 0:
+			return r
+
+def ponerficha(tablero,fila,col,pieza):
+	tablero[fila][col] = pieza
 
 def creartablero():
 	#Crea un array de 6 filas y 7 columnas con todos valores de 0
-	tablero = numpy.zeros((6,7))
+	tablero = numpy.zeros((cantidadfilas,cantidadcol))
 	return tablero
 
-
+#Da vuelta el tablero para que sea entendible
+def dibujartablero(tablero):
+	print(numpy.flip(tablero,0))
 
 tablero = creartablero()
 #print(tablero)
 
 findeljuego = False
-turno = 0
-
+turno = 1
+dibujartablero(tablero)
 while not findeljuego:
-	if turno==0:
-		jugada = int(input("Haz tu jugada Jugador 1 (0-6)"))
-		print(jugada)
-		print(type(jugada))
+	if turno==1:
+		col = int(input("Haz tu jugada Jugador 1 (0-6):"))
+		print(col)
+		if(posicionvalida(tablero,col)):
+			fila = proximafila(tablero,col)
+			ponerficha(tablero,fila,col,turno)
+			dibujartablero(tablero)
 
 	else:
-		jugada = int(input("Haz tu jugada Jugador 2 (0-6)"))
+		col = int(input("Haz tu jugada Jugador 2 (0-6):"))
+		if(posicionvalida(tablero,col)):
+			fila = proximafila(tablero,col)
+			ponerficha(tablero,fila,col,turno)
+			dibujartablero(tablero)
 
-	turno=0 if turno==1 else 1
+	turno=1 if turno==2 else 2
