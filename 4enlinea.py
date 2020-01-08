01a133cb9217d1407bcc8ec771cb5a201073ef64
Iván Berrutto <ivanberrutto@gmail.com>
2020-01-08 18:28:23 -0300

    Creacion de verificacion de victoria horizontal

--- 4enlinea.py
+++ 4enlinea.py
@@ -4,7 +4,7 @@ cantidadfilas=6
 cantidadcol=7
 #Verifica si se puede poner ficha en esa columna, entrega True si se puede(0)
 def posicionvalida(tablero, col):
-	return tablero[5][col] == 0
+	return tablero[cantidadfilas-1][col] == 0
 
 #Hace un for que busca la primera fila de la columna elegida que tenga un 0 ( no tenga ficha )
 def proximafila(tablero, col):
@@ -12,8 +12,8 @@ def proximafila(tablero, col):
 		if tablero[r][col] == 0:
 			return r
 
-def ponerficha(tablero,fila,col,pieza):
-	tablero[fila][col] = pieza
+def ponerficha(tablero,fila,col,turno):
+	tablero[fila][col] = turno
 
 def creartablero():
 	#Crea un array de 6 filas y 7 columnas con todos valores de 0
@@ -23,6 +23,18 @@ def creartablero():
 #Da vuelta el tablero para que sea entendible
 def dibujartablero(tablero):
 	print(numpy.flip(tablero,0))
+
+def movimientoganador(tablero,turno):
+	#Movimientos horizontales
+
+	#Hay que restarle 3 a la cantidad de columnas porque no puede comenzar un horizontal ganador en las ultimas 3 columnas
+	for c in range(cantidadcol-3):
+		for r in range(cantidadfilas):
+			if tablero[r][c] == turno and tablero[r][c+1] == turno and tablero[r][c+2] == turno and tablero[r][c+3] == turno:
+				return True
+
+
+
 
 tablero = creartablero()
 #print(tablero)
@@ -41,6 +53,9 @@ while not findeljuego:
 		fila = proximafila(tablero,col)
 		ponerficha(tablero,fila,col,turno)
 		dibujartablero(tablero)
+		if movimientoganador(tablero,turno):
+			print("Gano el jugador "+str(turno)+"!")
+			findeljuego=True
 
 	"""else:
 		col = int(input("Haz tu jugada Jugador 2 (0-6):"))
