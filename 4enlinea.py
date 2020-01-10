95ecc4e837a49577bb3ff8b012b9841ecc9c4af9
Iván Berrutto <ivanberrutto@gmail.com>
2020-01-10 06:09:04 -0300

    Rehago la funcion movimiento ganador de forma mucho mas optimizada ( falta testear un poco )

--- 4enlinea.py
+++ 4enlinea.py
@@ -26,8 +26,7 @@ def dibujartablero(tablero):
 	print(numpy.flip(tablero,0))
 
 
-def movimientoganador(tablero,turno):
-	asd=0
+"""def movimientoganador(tablero,fila,col,turno):
 	#Movimientos horizontales
 
 	#Hay que restarle 3 a la cantidad de columnas porque no puede comenzar un horizontal ganador en las ultimas 3 columnas
@@ -35,12 +34,15 @@ def movimientoganador(tablero,turno):
 		for r in range(cantidadfilas):
 			if tablero[r][c] == turno and tablero[r][c+1] == turno and tablero[r][c+2] == turno and tablero[r][c+3] == turno:
 				return True
+
+
 	#Movimientos verticales
 	for c in range(cantidadcol):
 		# Hay que restarle 3 a la cantidad de filas porque no puede comenzar un vertical ganador en las ultimas 3 filas
 		for r in range(cantidadfilas-3):
 			if tablero[r][c] == turno and tablero[r+1][c] == turno and tablero[r+2][c] == turno and tablero[r+3][c] == turno:
 				return True
+
 	#Diagonal positiva
 	for c in range(cantidadcol-3):
 		for r in range(cantidadfilas-3):
@@ -51,6 +53,82 @@ def movimientoganador(tablero,turno):
 		for r in range(cantidadfilas):
 			if tablero[r][c] == turno and tablero[r-1][c+1] == turno and tablero[r-2][c+2] == turno and tablero[r-3][c+3] == turno:
 				return True
+"""
+
+def movimientoganador(tablero,fila,col,turno):	
+	#Horizontal
+	contador=0
+	for c in range(cantidadcol):
+		if tablero[fila][c] == turno:
+			contador+=1
+		else:
+			contador=0
+		if contador==4:
+			return True
+	#Vertical
+	contador=0
+
+	for r in range(cantidadfilas):
+		if tablero[r][col] == turno:
+			contador+=1
+		else:
+			contador=0
+		if contador==4:
+			return True
+
+	x=1
+	while x:
+		if fila-x<0:
+			x-=1
+			break
+		if col-x<0:
+			x-=1
+			break
+		x+=1
+
+	#print(str(fila-x)+","+str(col-x))
+	contador=0
+	filaf=fila-x
+	colc=col-x
+	y=0
+	while True:
+		if(colc+y) >= cantidadcol or filaf+y >= cantidadfilas:
+			break
+		if tablero[filaf+y][colc+y] == turno:
+			contador+=1
+		if contador==4:
+			return True
+		y+=1
+	
+	#Diagonal positiva
+	z=1
+	while True:
+		if fila+z>=cantidadfilas:
+			#print("quebro f")
+			break
+		if col-z<0:
+			#print("quebro c")
+			break
+		z+=1
+	#print(str(fila+z-1)+","+str(col-z+1))
+	filaz= fila+z-1
+	colz= col-z+1
+
+	#Diagonal negativa
+	contador=0
+	n=0
+	while True:
+		if(colz+n) >= cantidadcol or (filaz-n) < 0:
+			break
+		if tablero[filaz-n][colz+n] == turno:
+			#print("dou")
+			contador+=1
+		if contador==4:
+			return True
+		n+=1
+
+
+
 
 
 tablero = creartablero()
@@ -70,7 +148,7 @@ while not findeljuego:
 		fila = proximafila(tablero,col)
 		ponerficha(tablero,fila,col,turno)
 		dibujartablero(tablero)
-		if movimientoganador(tablero,turno):
+		if movimientoganador(tablero,fila,col,turno):
 			print("Gano el jugador "+str(turno)+"!")
 			findeljuego=True
 
